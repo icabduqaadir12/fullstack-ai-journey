a599d5ffc2282c2066d810c8955a265452293caa
Abdukadir <icabduqaadir37@gmail.com>
2026-08-31 22:59:04 +0300

    Day22 processing

---
+++ month-01-javascript/Week-04/Day-22-ecommerce-order-analyzer/script.js
@@ -0,0 +1,52 @@
+const orders = [
+    {
+        id: 1001,
+        customer: "Ahmed",
+        city: "Nairobi",
+        items: [
+            { product: "Laptop", price: 800, quantity: 1 },
+            { product: "Mouse", price: 30, quantity: 2 }
+        ],
+        status: "delivered"
+    },
+    {
+        id: 1002,
+        customer: "Amina",
+        city: "Mogadishu",
+        items: [
+            { product: "Keyboard", price: 50, quantity: 1 },
+            { product: "Mouse", price: 30, quantity: 1 }
+        ],
+        status: "pending"
+    },
+    {
+        id: 1003,
+        customer: "Hassan",
+        city: "Nairobi",
+        items: [
+            { product: "Monitor", price: 300, quantity: 2 }
+        ],
+        status: "delivered"
+    },
+    {
+        id: 1004,
+        customer: "Fatima",
+        city: "Mombasa",
+        items: [
+            { product: "Headphones", price: 100, quantity: 1 },
+            { product: "Keyboard", price: 50, quantity: 2 }
+        ],
+        status: "cancelled"
+    },
+    {
+        id: 1005,
+        customer: "Mohamed",
+        city: "Nairobi",
+        items: [
+            { product: "Laptop", price: 800, quantity: 1 },
+            { product: "Headphones", price: 100, quantity: 1 }
+        ],
+        status: "delivered"
+    }
+];
+
